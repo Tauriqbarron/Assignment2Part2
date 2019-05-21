3c27d46c68baed64c13b981e7a031a8bd3fd8003
Tauriqbarron <tauriqbarron@hotmail.co.nz>
2019-05-21 19:52:57 +1200

    Finalish commit changed ini for Btn and on click for btn to method called from onc create

--- app/src/main/java/com/example/assignment2part2/MainActivity.java
+++ app/src/main/java/com/example/assignment2part2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.assignment2part2;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
@@ -8,6 +9,7 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -18,44 +20,49 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 
+
+
 public class MainActivity extends AppCompatActivity {
     private static int Request_Location = 99;
+    private Button btnGetLocation;
+    private String TAG = "Map App";
+    private String Lat,Lng;
+    private TextView txtLatitude,txtLongitude;
+    private FusedLocationProviderClient fusedLocationClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView txtLatitude =(TextView) findViewById(R.id.txtLatitude);
-        final TextView txtLongitude =(TextView) findViewById(R.id.txtLongitude);
-        final Button btnGetLocation = (Button) findViewById(R.id.btnGetLocation);
-        final FusedLocationProviderClient client = LocationServices.getFusedLocationProviderClient(this);
+        txtLatitude =(TextView) findViewById(R.id.txtLatitude);
+        txtLongitude =(TextView) findViewById(R.id.txtLongitude);
+        displayLocation(this);
+    }
+    public void displayLocation(Context context){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this,new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION},Request_Location);
+        }
+        btnGetLocation = findViewById(R.id.btnGetLocation);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(context);
         btnGetLocation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 try{
-                    final Task<Location> location = client.getLastLocation();
-                    location.addOnCompleteListener(new OnCompleteListener<Location>() {
+                    final Task<Location> currentLocation =fusedLocationClient.getLastLocation();
+                    currentLocation.addOnCompleteListener(new OnCompleteListener<Location>() {
                         @Override
                         public void onComplete(@NonNull Task<Location> task) {
-                            if(location !=null) {
-                                txtLatitude.setText((Double.toString(task.getResult().getLatitude())));
-                                txtLongitude.setText((Double.toString(task.getResult().getLongitude())));
-                                System.err.println(task.getResult().getLatitude());
-                            }
+                         Lat = Double.toString(currentLocation.getResult().getLatitude());
+                         Lng = Double.toString(currentLocation.getResult().getLongitude());
+                         txtLatitude.setText(Lat);
+                         txtLongitude.setText(Lng);
                         }
                     });
-                }catch(SecurityException ex){
-                    ex.printStackTrace();
+                }catch (SecurityException e){
+                    Log.e(TAG,"Get Location: Security Exception: "+e.getMessage());
                 }
             }
         });
-        LocationRequest req = new LocationRequest();
-        req.setInterval(2000);
-        req.setFastestInterval(500);
-        req.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
-        != PackageManager.PERMISSION_GRANTED){
-            ActivityCompat.requestPermissions(this,new String[]{
-                    Manifest.permission.ACCESS_FINE_LOCATION},Request_Location);
-        }
     }
 }
